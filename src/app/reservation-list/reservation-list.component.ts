ff51a2ace33c6b123849cc1de272778a831f5469
Illia Andreyeu <k2andreev@gmail.com>
2025-01-18 15:27:36 +0100

    Implement the Hotel reservation feature

---
+++ src/app/reservation-list/reservation-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { ReservationService } from '../reservation/reservation.service';
+import { Reservation } from '../models/reservation';
+
+@Component({
+  selector: 'app-reservation-list',
+  templateUrl: './reservation-list.component.html',
+  styleUrls: ['./reservation-list.component.css'],
+})
+export class ReservationListComponent implements OnInit {
+  reservations: Reservation[] = [];
+
+  constructor(private reservationService: ReservationService) {}
+
+  ngOnInit(): void {
+    this.reservations = this.reservationService.getReservations();
+    console.log(this.reservations);
+  }
+
+  deleteReservation(id: string) {
+    this.reservationService.deleteReservation(id);
+  }
+}
